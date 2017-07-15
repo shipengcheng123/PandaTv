7460a842356a2b82a717b2ddeabff8d4ec673689
zhangxin123456 <398101150@qq.com>
2017-07-15 10:27:27 +0800

    资源文件

---
+++ app/src/main/java/jiyun/com/ipandatv/fragment/Home/tile_right/Title_Login.java
@@ -0,0 +1,30 @@
+package jiyun.com.ipandatv.fragment.Home.tile_right;
+
+import jiyun.com.ipandatv.R;
+import jiyun.com.ipandatv.base.BaseActivity;
+
+/**
+ * Created by lx on 2017/7/14.
+ */
+
+public class Title_Login extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_login;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+}
---
+++ app/src/main/java/jiyun/com/ipandatv/fragment/Home/tile_right/Title_Setting.java
@@ -0,0 +1,30 @@
+package jiyun.com.ipandatv.fragment.Home.tile_right;
+
+import jiyun.com.ipandatv.R;
+import jiyun.com.ipandatv.base.BaseActivity;
+
+/**
+ * Created by lx on 2017/7/14.
+ */
+
+public class Title_Setting extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.panda_person_setting;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+}
