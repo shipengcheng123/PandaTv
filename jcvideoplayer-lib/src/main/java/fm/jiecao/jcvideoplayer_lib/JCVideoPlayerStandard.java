58c0f04b551e6914b9dfc63e3d78307055693f67
lsylph <924619261@qq.com>
2017-07-21 20:04:14 +0800

    马佳兴   视频代码完善

--- jcvideoplayer-lib/src/main/java/fm/jiecao/jcvideoplayer_lib/JCVideoPlayerStandard.java
+++ jcvideoplayer-lib/src/main/java/fm/jiecao/jcvideoplayer_lib/JCVideoPlayerStandard.java
@@ -80,6 +80,7 @@ public class JCVideoPlayerStandard extends JCVideoPlayer {
             @Override
             public void onClick(View v) {
 
+
                 View inflate = View.inflate(context, R.layout.pupowdbju, null);
                 PopupWindow popupWindow=new PopupWindow(inflate,WindowManager.LayoutParams.WRAP_CONTENT,WindowManager.LayoutParams.WRAP_CONTENT,true);
                 popupWindow.setOutsideTouchable(true);
