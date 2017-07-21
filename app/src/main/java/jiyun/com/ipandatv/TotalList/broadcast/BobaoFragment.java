0f2118746eaa438d57cec844182a5e403da9efeb
lsylph <924619261@qq.com>
2017-07-21 15:09:33 +0800

    lsy

--- app/src/main/java/jiyun/com/ipandatv/TotalList/broadcast/BobaoFragment.java
+++ app/src/main/java/jiyun/com/ipandatv/TotalList/broadcast/BobaoFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,16 +37,14 @@ import jiyun.com.ipandatv.utils.MyLog;
  * Created by Lenovo on 2017/7/12.
  */
 
-public class BobaoFragment extends BaseFragment implements BobaoContract.View{
+public class BobaoFragment extends BaseFragment implements BobaoContract.View {
 
 
-    @BindView(R.id.Personal_Cente)
-    ImageView PersonalCente;
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
     @BindView(R.id.bobao_RecyclerView)
     PullToRefreshRecyclerView mRecyclerView;
     Unbinder unbinder;
+    @BindView(R.id.Personal_Cente)
+    ImageView PersonalCente;
     private ImageView mImage;
     private TextView title;
     private View view1;
@@ -61,6 +58,7 @@ public class BobaoFragment extends BaseFragment implements BobaoContract.View{
     private String url;
     private Handler handleProgress = new Handler();
     private ProgressDialog progressDialog = null;
+
     @Override
     protected int getLayoutId() {
         return R.layout.bobao_fragment;
@@ -77,10 +75,9 @@ public class BobaoFragment extends BaseFragment implements BobaoContract.View{
             @Override
             public void onClick(View v) {
 
-
-                Intent intent = new Intent(getContext(),WebActivity.class);
-                intent.putExtra("url",url);
-                MyLog.e("URL",url);
+                Intent intent = new Intent(getContext(), WebActivity.class);
+                intent.putExtra("url", url);
+                MyLog.e("URL", url);
                 startActivity(intent);
             }
         });
@@ -89,7 +86,7 @@ public class BobaoFragment extends BaseFragment implements BobaoContract.View{
 
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(App.activity,DividerItemDecoration.VERTICAL));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(App.activity, DividerItemDecoration.VERTICAL));
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setPullRefreshEnabled(true);//下拉刷新
         //是否开启上拉加载功能
@@ -127,15 +124,14 @@ public class BobaoFragment extends BaseFragment implements BobaoContract.View{
         });
 
 
-
-        bobaoAdapter = new BobaoAdapter(getContext(),mList);
+        bobaoAdapter = new BobaoAdapter(getContext(), mList);
 
 
     }
 
     @Override
     protected void loadData() {
-        progressDialog = ProgressDialog.show(App.activity,"请稍等...","获取数据中...",true);
+        progressDialog = ProgressDialog.show(App.activity, "请稍等...", "获取数据中...", true);
         bobaoPresenter = new BobaoPresenter(this);
         presenter.start();
         handleProgress.post(new Runnable() {
@@ -206,7 +202,7 @@ public class BobaoFragment extends BaseFragment implements BobaoContract.View{
 
     @Override
     public void setBasePresenter(BobaoContract.Presenter presenter) {
-        this.presenter=presenter;
+        this.presenter = presenter;
     }
 
 }
