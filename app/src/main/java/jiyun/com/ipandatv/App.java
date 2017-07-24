6601e95484fff222edee6a5e279d58e97fa489a6
lsylph <924619261@qq.com>
2017-07-24 11:40:24 +0800

    马佳兴  历史记录   收藏

--- app/src/main/java/jiyun/com/ipandatv/App.java
+++ app/src/main/java/jiyun/com/ipandatv/App.java
@@ -7,7 +7,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.widget.RadioGroup;
 
-import com.j256.ormlite.dao.Dao;
 import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -50,7 +49,7 @@ public class App extends Application {
         MobclickAgent.openActivityDurationTrack(false);
         Config.DEBUG = true;
         UMShareAPI.get(this);
-        Logger.d(TAG, "[ExampleApplication] onCreate");
+//        Logger.d(TAG, "[ExampleApplication] onCreate");
         JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
         JPushInterface.init(this);     		// 初始化 JPush
 //        CrashHandler.getInstance().init(this);//初始化全局异常管理
